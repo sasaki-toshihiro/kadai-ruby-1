453f2560e6879094795bf5facbd33b6eac6c4df4
toshihiro.sasaki <toshihiro.sasaki@g.softbank.co.jp>
2021-08-03 11:20:31 +0000

    update variable

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,11 +1,9 @@
 
-  Person = {
-    lastName: "佐々木",
-    firstName: "敏博",
-    myAge: 47
-  }
-  
-  sayHello = Person[:lastName] + Person[:firstName] + 'です。 ' + Person[:myAge].to_s + 'です。'
+  lastName = "佐々木"
+  firstName = "敏博"
+  myAge = 47
+
+  sayHello = lastName + firstName + 'です。 ' + myAge.to_s + 'です。'
 
   puts sayHello
 
